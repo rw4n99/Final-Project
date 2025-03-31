f227d6d9964a88a0816cc3e5d4b2c14000041be1
Rowan Godfrey <rowangodfrey2@gmail.com>
2025-03-31 14:53:00 +0100

    Created basic homepage template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  def hello
-    render html: "Hello World!"
+  def index
+    @journuals = Journal.all
   end
 end
--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,10 +1,24 @@
-class EntriesController < ApplicationController
+class JournalsController < ApplicationController
     def index
+        @journals = Journal.all
     end
-
+        
     def new
+        @journal = Journal.new()
     end
-
+        
     def create
+        @journal = Journal.new(journal_params)
+       if @journal.save
+           redirect_to root_url
+       else
+           render :new
+       end
+
+       private
+
+    def journal_params
+        params.require(:journal).permit(:name)
+    end
     end
 end
