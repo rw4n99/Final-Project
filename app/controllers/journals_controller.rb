9da7773b89c9700e81dd4ab2f2f9c7d3c56b5dd7
Rowan Godfrey <rowangodfrey2@gmail.com>
2025-04-05 17:21:40 +0100

    Moved list of documents to homepage2

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,13 +1,15 @@
 class JournalsController < ApplicationController
     def index
-        
+      if user_signed_in?
         @visa_document = VisaDocument.find_by(user_id: current_user.id)
-        
+        puts "user in"
+      else
+        @visa_document = nil
+      end
     end
-        
+  
     def new
     end
-
+  
     private
-
-end
+  end
