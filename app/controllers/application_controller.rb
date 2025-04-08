e3034a5dd9f975a89a5046037d92cb5f3078f622
Rowan Godfrey <rowangodfrey2@gmail.com>
2025-04-08 09:41:07 +0100

    Added wels support button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
-  default_form_builder GOVUKDesignSystemFormBuilder::FormBuilder
+  before_action :set_locale
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  private
+
+  def set_locale
+    if params[:locale].blank?
+      redirect_to url_for(locale: I18n.default_locale) and return
+    else
+      I18n.locale = params[:locale]
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,31 @@
 Rails.application.routes.draw do
-  get "pages/new"
-  # Devise for authentication
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  scope "(:locale)", locale: /en|cy/ do
+    get "pages/new"
+    # Devise for authentication
+    devise_for :users, controllers: { registrations: 'users/registrations' }
 
-  # Root path (Pick one)
-  root 'journals#index' # or change to 'visa_applications#new' if needed
+    # Root path (Pick one)
+    root 'journals#index' # or change to 'visa_applications#new' if needed
 
-  # Visa Applications Routes (only necessary actions)
-  resources :visa_documents, only: [:new, :create, :edit, :update, :destroy, :index]
+    # Visa Applications Routes (only necessary actions)
+    resources :visa_documents, only: [:new, :create, :edit, :update, :destroy, :index]
 
-  # Journals Routes
-  resources :journals, only: [:create, :new, :destroy]
+    # Journals Routes
+    resources :journals, only: [:create, :new, :destroy]
 
-  get 'pages/new', to: 'pages#new'
-  # Or, if you want it to be the home page
-  # root 'pages#new'
+    get 'pages/new', to: 'pages#new'
+    # Or, if you want it to be the home page
+    # root 'pages#new'
 
-  # Health check
-  get "up" => "rails/health#show", as: :rails_health_check
+    # Health check
+    get "up" => "rails/health#show", as: :rails_health_check
 
-  # PWA Routes
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+    # PWA Routes
+    get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+    get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Set cookies
-  post "/form-handler", to: "cookies#set_cookie"
+    # Set cookies
+    post "/form-handler", to: "cookies#set_cookie"
+
+  end
 end
